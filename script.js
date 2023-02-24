b2b83b1505f36e4f48b993d6930dfa67b0c8194f
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-02-24 09:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let ans = 0;
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  //   let price = document.getElementsByClassName("price");
+  let price = document.querySelectorAll(".price");
+  console.log(price);
+  price.forEach((ele) => {
+    ans += parseInt(ele.innerHTML);
+  });
+  document.getElementById("result").innerHTML = ans;
 };
 
 getSumBtn.addEventListener("click", getSum);
-

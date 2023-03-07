f20ae683e1fc38fa167199236e09d491f0f9b439
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-03-07 19:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let ans = 0;
+
 const getSum = () => {
-  //Add your code here
-  //   let price = document.getElementsByClassName("price");
-  let price = document.querySelectorAll(".price");
-  console.log(price);
-  price.forEach((ele) => {
-    ans += parseInt(ele.innerHTML);
-  });
-  let tbody = document.getElementsByTagName("tbody");
-  let tr = document.createElement("tr");
-  let td = document.createElement("td");
-  let td1 = document.createElement("td");
-  td.innerHTML = "Total";
-  td.id = "total";
-  td1.id = "result";
-  tr.append(td);
-  tr.append(td1);
-  tbody[0].append(tr);
-  document.getElementById("result").innerHTML = ans;
+//Add your code here
+	let prices = document.querySelectorAll('.price');
+	let sum = 0;
+	prices.forEach(price => {
+		sum += parseFloat(price.textContent);
+	});
+	document.getElementById('table').innerHTML += `
+	   <tr id="ans">
+          <td></td>
+          <td>${sum}</td>
+        </tr>`
 };
 
-getSumBtn.addEventListener("click", getSum);
-
-// let tr = `<tr>
-// <td id="total">Total</td>
-//  <td id="result"></td>
-// </tr>`;
-// tbody.innerHTML = tr;
+getSumBtn.addEventListener("click", getSum)
